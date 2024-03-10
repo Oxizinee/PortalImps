345bc2ace99b503ad0f85b41e5297c3f12b64036
Aleksandra Zupanska <aleksandra.zupanska@student.howest.be>
2024-03-10 19:19:57 +0100

    simple nav mesh for imps

--- Imps/Assets/Scripts/ImpMovement.cs
+++ Imps/Assets/Scripts/ImpMovement.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class ImpMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform Player;
+
+    private CharacterController _characterController;
+    private NavMeshAgent _agent;
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _agent.SetDestination(Player.transform.position);
+
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y - (transform.localScale.y / 2), transform.position.z), -Vector3.up, 0.04f))
+        {
+        }
+        else
+        {
+            transform.position -= (Vector3.up * 2f) * Time.deltaTime;
+        }
     }
 }
